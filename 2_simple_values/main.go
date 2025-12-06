70583d766cd2255d93a81beeadb27c05d76b9003
Pankaj Agarwal <drawishu@gmail.com>
2025-12-06 18:17:03 +0530

    simple variables

---
+++ 2_simple_values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// int
+	fmt.Println(1)
+
+	fmt.Println("hello using string")
+	fmt.Println(true)
+	fmt.Println(false)
+
+	fmt.Println(10.5)
+	fmt.Println(7.0/3.0)
+	fmt.Println(7/3)
+	fmt.Println(7.0/3)
+	fmt.Println(7/3.0)
+}
